f8790eacb7a365aad532655eaed2ce55747a91d4
Andrei Zhuk <andrei.zhuk@sidion.de>
2020-12-01 00:40:30 +0300

    starting over again :)

---
+++ src/test/java/com/test/article_api/ArticleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.article_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
